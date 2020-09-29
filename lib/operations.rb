ce31844eccb59e148b39bd2932398228265a2c84
ocawthorne <ocawthorne@gmail.com>
2020-09-29 18:39:11 +0100

    operations-092920

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+    if speed < 40
+        true
+    elsif speed > 60
+        true
+    else
+        false
+    end
 end
-
-
 
 def not_safe?(speed)
-	
+    safe = speed < 40 ? true : (speed > 60 ? true : false)
 end
-	
-
-
